d3bde36cfe6935c003a325dbdafc68971ccf4ac3
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-09-09 19:20:59 +0300

    TMP_2 push

---
+++ TMP/lesson_1/Monastyrskiy_c21-703_1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define N 20
+#define ll long long
+int main(int argc, char *argv[]){
+int i,s,a[N],j,k;
+/* if(argc > 1)
+srand(atoi(argv[1])); */
+srand(time(NULL));
+for(i=0;i<N;i++){
+a[i]=rand()%100;
+printf("a[%d]=  %d\n",i,a[i]);
+}
+
+printf("\n\n");
+
+for (i=0; i < N ; i++){
+for (j = 0; j <i ; j++){
+if(a[i] > a[j]) {
+k=a[i];
+a[i] = a[j];
+a[j] = k;
+
+
+}
+}
+}
+for(i=0; i<N; i++){
+printf("a[%d] = %d\n",i,a[i]);
+}
+return 0;
+}
