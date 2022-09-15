00a5b2c07bef7d926342b736c6a72c21ee77e25d
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-09-15 21:34:36 +0300

    Work in progress

---
+++ Java_OOP/Monastyrskiy/JTest/src/main/java/org/Constants.java
@@ -0,0 +1,12 @@
+package org;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.json.*;
+
+
+public final class Constants {
+    public Constants(){
+        public PL PL[] = new PL[100];
+    }
+}
