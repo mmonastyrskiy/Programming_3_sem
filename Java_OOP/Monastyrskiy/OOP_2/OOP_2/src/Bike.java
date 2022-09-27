f445b629a962d90225dff77e18789aa887d4bd0c
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-09-27 08:45:28 +0300

    OOP_2 push

--- Java_OOP/Monastyrskiy/OOP_2/OOP_2/src/Bike.java
+++ Java_OOP/Monastyrskiy/OOP_2/OOP_2/src/Bike.java
@@ -3,10 +3,29 @@ class Wheel {
     Wheel (int di){
         d = di;
     }
-}
+    public int hashCode(){
+        final int prime = 53;
+        return prime + this.d;
+    }
+
+    public boolean equals(Object object){
+        if(this == object){return true;}
+        if(object == null){return false;}
+        if (this.getClass() != object.getClass())
+            return false;
+        Wheel other = (Wheel) object;
+        return other.d == this.d;
+    }
+    }
 public class Bike implements Cloneable {
     int wheelCount;
     Wheel w;
+
+    public int hashCode(){
+        final int prime = 59;
+        final int constant = 2;
+        return prime * constant + this.w.hashCode() + wheelCount;
+    }
 
     public boolean equals(Object object){
         if(this == object){return true;}
@@ -14,19 +33,14 @@ public class Bike implements Cloneable {
         if (this.getClass() != object.getClass())
             return false;
         Bike other = (Bike) object;
-        if(!(this.w.equals(other.w))&& !(this.wheelCount == other.wheelCount)){
-            
-        }
-
-
-        return true;
+        return this.w.equals(other.w) && this.wheelCount == other.wheelCount;
     }
 
 
-    public Bike clone(){
-        Bike result = new Bike(this.wheelCount, this.w.d);
-        result.w = new Wheel(this.w.d);
-        return result;
+    public Bike clone()throws CloneNotSupportedException{
+        Bike clone = (Bike) super.clone();
+        clone.w = new Wheel(this.w.d);
+        return clone;
 
     }
     Bike(int wCount, int wDiameter) {
@@ -36,14 +50,16 @@ public class Bike implements Cloneable {
 
 
 
-    public static void main (String[] arg) throws CloneNotSupportedException{
+    public static void main (String[] args) throws CloneNotSupportedException {
         Bike bike1 = new Bike(2, 26);
-        Bike bike2 = (Bike) bike1.clone();
+        Bike bike2 = bike1.clone();
         bike1.wheelCount = 1;
         bike1.w.d = 29;
-        System.out.println (bike2.wheelCount);
-        System.out.println (bike2.w.d);
+
         System.out.println (bike1.wheelCount);
         System.out.println (bike1.w.d);
+        System.out.println("Clone:");
+        System.out.println (bike2.wheelCount);
+        System.out.println (bike2.w.d);
     }
 }
