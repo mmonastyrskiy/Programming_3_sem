6f078be13d270de8e2c1d3e958a9c9f052315cbc
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-10-08 23:24:42 +0300

    Java OOP_4_1

---
+++ Java_OOP/Monastyrskiy/OOP_3/OOP_3_1/OOP_3_1/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+class abcSort implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.compareToIgnoreCase(o2);
+    }
+}
+class StringLengthSort implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        if(o1.length() > o2.length()){
+            return 1;
+        }else{
+            if(o1.length() < o2.length()){
+                return -1;
+            }else{
+                return 0;
+            }
+        }
+    }
+}
+public class Main {
+     static String str = "Strings are constant; their values cannot be changed \n" +
+             "after they are created.";
+     public static String sortWords(String inpStr){
+         String[] s = inpStr.split(" ");
+         Arrays.sort(s);
+         Comparator<String> abc = new abcSort();
+         Comparator<String> stringLengthComparator = new StringLengthSort();
+         Arrays.sort(s,abc.thenComparing(stringLengthComparator));
+         for(String st:s ){
+             System.out.println(st);
+         }
+         return Arrays.toString(s);
+
+     };
+     public static void main(String[] args){
+         sortWords(str);
+
+     }
+}
