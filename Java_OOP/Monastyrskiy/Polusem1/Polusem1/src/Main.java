40d6b32814a89a5d64dc300cba09a498c2764ff7
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-10-22 16:40:07 +0300

    Java polusem

---
+++ Java_OOP/Monastyrskiy/Polusem1/Polusem1/src/Main.java
@@ -0,0 +1,31 @@
+class Phone_book{
+    Record[] Contacts;
+
+
+    private void Add_record(String name,Integer PN){} // ф-я добавления в телефонную книгу
+    private void Del_record(Record record) {}// ф-я удаления
+    private void Del_record(Integer phone) {}// ф-я удаления
+    private void Del_record(String name) {}// ф-я удаления
+    private Record Search (String name){
+        /*код поиска по имени*/
+        return Record;
+    }
+    private Record Search (Integer PN){
+        /*код поиска по телефону*/
+        return Record;
+    }
+}
+
+final class Record{
+String name;
+Integer Phone_Number;
+public Record(String name, Integer Phone_Number){
+}
+
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
