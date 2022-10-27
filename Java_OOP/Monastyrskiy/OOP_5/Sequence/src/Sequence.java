c48c1eaa4d14e565d7517a3abe24006d0508c8ba
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-10-27 20:40:13 +0300

    Java OOP Sequence push

--- Java_OOP/Monastyrskiy/OOP_5/Sequence/src/Sequence.java
+++ Java_OOP/Monastyrskiy/OOP_5/Sequence/src/Sequence.java
@@ -1,30 +1,97 @@
 interface Selector {
     boolean end();
+
     Object current();
+
     void next();
 }
+
 public class Sequence {
     private Object[] items;
     private int next = 0;
+    private int next2 = 10;
+
     public Sequence(int size) {
         items = new Object[size];
     }
+
     public void add(Object x) {
         if (next < items.length)
             items[next++] = x;
     }
-    public static void main(String[] args) {
-        Sequence sequence = new Sequence(10);
-        for (int i = 0; i < 10; i++)
-            sequence.add(Integer.toString(i));
-        Selector selector = sequence.getSelector();
+
+    public void down(Object y) {
+        if (next2 > 0)
+            items[--next2] = y;
+    }
+
+
+    public static void getSelector(Sequence sequence) {
+        Selector selector = new Selector() {
+            private int i = 0;
+
+            @Override
+            public boolean end() {
+                return i == sequence.items.length;
+            }
+
+            @Override
+            public Object current() {
+                return sequence.items[i];
+            }
+
+            @Override
+            public void next() {
+                if (i < sequence.items.length)
+                    i++;
+            }
+        };
+
         while (!selector.end()) {
             System.out.print(selector.current() + " ");
             selector.next();
         }
+
+    }
+
+
+    public static void getSelectorReverse(Sequence sequence) {
+        Selector selector2 = new Selector() {
+            private int i = 0;
+
+            @Override
+            public boolean end() {
+                return i == sequence.items.length;
+            }
+
+            @Override
+            public Object current() {
+                return sequence.items[i];
+            }
+
+            @Override
+            public void next() {
+                if (i < sequence.items.length)
+                    i++;
+
+
+            }
+        };
+        while (!selector2.end()) {
+            System.out.print(selector2.current() + " ");
+            selector2.next();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(10);
+        for (int i = 0; i < 10; i++)
+            sequence.add(Integer.toString(i));
+        getSelector(sequence);
         System.out.println();
-        selector = sequence.getSelectorReverse();
-        while (!selector.end()) {
-            System.out.print(selector.current() + " ");
-            selector.next();
-        }}}
+        for (int i = 0; i < 10; i++)
+            sequence.down(Integer.toString(i));
+        getSelectorReverse(sequence);
+    }
+}
