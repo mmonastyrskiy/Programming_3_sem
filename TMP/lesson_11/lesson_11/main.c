5b27f266b1a88a57a5cd99274de5f75904ab6bce
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-10-28 23:11:27 +0300

    TMP lesson 11

---
+++ TMP/lesson_11/lesson_11/main.c
@@ -0,0 +1,125 @@
+#include "stack.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+void addition(stack* head_ptr){
+    int operator_a;
+    int operator_b;
+    if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_a = (int)pop(head_ptr)-48;
+        if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_b =  (int)pop(head_ptr)-48;
+    printf("%d+%d\n",operator_b,operator_a);
+    push(head_ptr,(char) (operator_a + operator_b)+48);
+}
+void subtraction (stack* head_ptr){
+    int operator_a;
+    int operator_b;
+    if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_a =  (int)pop(head_ptr)-48;
+        if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_b =  (int)pop(head_ptr)-48;
+    printf("%d-%d\n",operator_b,operator_a);
+    push(head_ptr,(char) (operator_b - operator_a)+48);
+}
+
+void multiplication (stack* head_ptr){
+    int operator_a;
+    int operator_b;
+    if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_a =  (int)pop(head_ptr)-48;
+        if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_b =  (int)pop(head_ptr)-48;
+    printf("%d*%d\n",operator_b,operator_a);
+    push(head_ptr,(char) (operator_b * operator_a)+48);
+}
+
+void division (stack* head_ptr){
+    int operator_a;
+    int operator_b;
+    if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_a =  (int)pop(head_ptr)-48;
+        if(IsClean(head_ptr)=='0'){
+        printf("error empty stack found");
+        exit(2);
+    }
+    operator_b =  (int)pop(head_ptr)-48;
+    if (operator_a == 0)  return;
+    printf("%d/%d\n",operator_b,operator_a);
+    push(head_ptr,(char) (operator_b / operator_a)+48);
+}
+
+
+void calc(stack* head_ptr,const char* equasion){
+    char cur;
+    char res;
+    int i;
+    for (i = 0; equasion[i] != '\0'; i++){
+        cur = equasion[i];
+        /*printf("%c:%d\n",cur,isdigit(cur));*/
+        if(isdigit(cur)>0){
+            push(head_ptr,cur);
+           /* printf("\n%c",head_ptr->next->i);*/
+        }
+        else if((cur == '+') ||(cur == '-') || (cur == '*') || (cur == '/'))
+        {
+            if(cur == '+'){
+                addition(head_ptr);
+            }
+            else if (cur == '-'){
+                subtraction(head_ptr);
+
+            }
+            else if(cur =='*'){
+                multiplication(head_ptr);
+            }
+            else if (cur == '/'){
+                division(head_ptr);
+            }
+
+        }
+        else{continue;}
+
+    }
+    res = pop(head_ptr);
+    printf("%c\n",res);
+
+}
+int main(int argc, const char** argv){
+    stack* head_ptr;
+    stack* head = NULL;
+    if((argc < 2) || (argc > 3)){
+        printf("bad argument");
+        exit(3);
+    }
+    head_ptr = malloc(sizeof(stack));
+    if(head_ptr == NULL){
+        printf("Malloc error\n");
+        exit(1);
+    }
+    head_ptr -> next = head;
+    calc(head_ptr,argv[1]);
+    return 0;
+
+} 
---
+++ TMP/lesson_11/lesson_11/stack.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Stack
+{
+	char i;
+	struct Stack* next;
+} stack;
+
+void push(stack* head, char new_head_data);
+char pop(stack* head_ptr);
+char  top(stack* head);
+char IsClean(stack* head_ptr);
