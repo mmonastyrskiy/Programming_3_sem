788f36e1529b944bef780c09acf9b5a4938c8e90
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-11-04 19:38:21 +0300

    Zmeyka stack

--- TMP/lesson_12/snake/snake/snake/snake_head/gamelogic.h
+++ TMP/lesson_12/snake/snake/snake/snake_head/gamelogic.h
@@ -4,12 +4,13 @@
 #define X_REZ 80
 #define Y_REZ 24
 
-struct termios orig_termios;
+
 typedef struct Snake{
     int x,y,vel_y,vel_x;
     int score;
     char repr;
     struct Snake* next;
+    struct Snake* prev;
 } snake;
 
 
@@ -27,14 +28,14 @@ void Move(char** field,snake* s);
 void SnakeMover(char** field,snake* s,apple* a); /*Общий метод движения змеи*/
 char isSnake(char** field,int x,int y); /* содержит ли клетка змею? '0' - True '1'*/
 char isApple(char** field,int x, int y); /* содержит ли клетка яблоко ? '0' - True '1'*/
-char isHead(snake* s); /* Является ли участок хвостом змеи '0' - True , '1' - False*/
+char isTail(snake* s); /* Является ли участок хвостом змеи '0' - True , '1' - False*/
 char isEmpty(char** field, int x, int y );  /*Является ли клетка пустой '0' - True , '1' - False*/
 void Grow(char** field,snake* s); /*Увеличить питона*/
 void Eat(char** field,snake* s, apple* a); /*Кушоц*/
 void clear(); /* Очистить экран */
  void SpawnSnake(char** field,snake* s);
  void ticker(char ** field, apple* a, snake* s);
-
+char isTailCRD(char** f,int x,int y);
 
 void printfield(char** f);
 
