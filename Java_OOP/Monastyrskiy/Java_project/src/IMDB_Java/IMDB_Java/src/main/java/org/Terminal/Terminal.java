608c2ae1cec54e46f70169a97c6daa0396845c36
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-11-17 21:11:09 +0300

    Java Terminal

--- Java_OOP/Monastyrskiy/Java_project/src/IMDB_Java/IMDB_Java/src/main/java/org/Terminal/Terminal.java
+++ Java_OOP/Monastyrskiy/Java_project/src/IMDB_Java/IMDB_Java/src/main/java/org/Terminal/Terminal.java
@@ -1,5 +1,5 @@
 package org.Terminal;
-
+import org.Searcher.*;
 public class Terminal{
     public Terminal(){
 
@@ -7,34 +7,35 @@ public class Terminal{
     private void quit(){
         System.exit(0);
     }
-    private void Save(String query){
+    private void Save(String[] query){
+
 
     }
-    private void Load(String query){
+    private void Load(String[] query){
 
     }
-    private void Search(String query){
+    private void Search(String[] query){
 
     }
-    private void Stat(String query){
+    private void Stat(String[] query){
 
     }
-    private void Add(String query){
+    private void Add(String[] query){
 
     }
-    private void Del(String query){
+    private void Del(String[] query){
 
     }
     public String[] ParceQuery(String query){
         String[] cmd = query.toLowerCase().split(" ");
         switch (cmd[0]){
             case "q":{quit();return cmd;}
-            case "save":{Save(query);return cmd;}
-            case "load":{Load(query);return cmd;}
-            case "search":{Search(query);return cmd;}
-            case "stat":{Stat(query); return  cmd;}
-            case "add":{Add(query);return cmd;}
-            case "del":{Del(query);return cmd;}
+            case "save":{Save(cmd);return cmd;}
+            case "load":{Load(cmd);return cmd;}
+            case "search":{Search(cmd);return cmd;}
+            case "stat":{Stat(cmd); return  cmd;}
+            case "add":{Add(cmd);return cmd;}
+            case "del":{Del(cmd);return cmd;}
             default: return cmd;
 
 
