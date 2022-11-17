ee60e2120a92e1011881a2de5d0ffdae4274991d
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-11-17 20:38:37 +0300

    Java Main

--- Java_OOP/Monastyrskiy/Java_project/src/IMDB_Java/IMDB_Java/src/main/java/org/IMDB_Main/Main.java
+++ Java_OOP/Monastyrskiy/Java_project/src/IMDB_Java/IMDB_Java/src/main/java/org/IMDB_Main/Main.java
@@ -3,5 +3,21 @@ import org.Person.*;
 import org.Film.*;
 import org.Saveable.Saveable;
 import org.Searcher.Searcher;
+import org.Terminal.*;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String query;
+        Terminal terminal = new Terminal();
+        while (true){
+            System.out.print("> ");
+            query = scanner.nextLine();
+            terminal.ParceQuery(query);
+
+        }
+
+    }
 }
--- Java_OOP/Monastyrskiy/Java_project/src/IMDB_Java/IMDB_Java/src/main/java/org/Terminal/Terminal.java
+++ Java_OOP/Monastyrskiy/Java_project/src/IMDB_Java/IMDB_Java/src/main/java/org/Terminal/Terminal.java
@@ -1,1 +1,43 @@
+package org.Terminal;
 
+public class Terminal{
+    public Terminal(){
+
+    }
+    private void quit(){
+        System.exit(0);
+    }
+    private void Save(String query){
+
+    }
+    private void Load(String query){
+
+    }
+    private void Search(String query){
+
+    }
+    private void Stat(String query){
+
+    }
+    private void Add(String query){
+
+    }
+    private void Del(String query){
+
+    }
+    public String[] ParceQuery(String query){
+        String[] cmd = query.toLowerCase().split(" ");
+        switch (cmd[0]){
+            case "q":{quit();return cmd;}
+            case "save":{Save(query);return cmd;}
+            case "load":{Load(query);return cmd;}
+            case "search":{Search(query);return cmd;}
+            case "stat":{Stat(query); return  cmd;}
+            case "add":{Add(query);return cmd;}
+            case "del":{Del(query);return cmd;}
+            default: return cmd;
+
+
+        }
+    }
+}
