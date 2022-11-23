376b4dc99f3b7bc6997bf4f92f8b0770d5c1558a
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-11-23 15:59:31 +0300

    Update lab_5_9.sql

--- DataBases/made/lab_5_9.sql
+++ DataBases/made/lab_5_9.sql
@@ -1,4 +1,5 @@
 --lab4
+
 --7
 CREATE VIEW dep_staff_counts AS
 SELECT department,count(distinct e.employee_id)ecount
@@ -11,7 +12,7 @@ SELECT * FROM bd_employees where phone_number::TEXT ~* '^\d+$';
 
 --2
 SELECT * FROM bd_employees e join bd_departments deps
-on(deps.id = e.department_id) where (street ~*'[^A-Z\s]')
+on(deps.id = e.department_id) where (street ~*'[^A-Z\s]');
 
 --3
 SELECT (last_name,REGEXP_REPLACE(phone_number,'^\+7','8')) FROM bd_employees;
@@ -38,11 +39,31 @@ REVOKE ALL ON bd_employees from my_colleague;
 REVOKE ALL ON bd_departments from my_colleague;
 DROP ROLE my_colleague;
 CREATE ROLE my_colleague;
-GRANT UPDATE, DELETE ON bd_departments to my_colleague
+GRANT UPDATE, DELETE ON bd_departments to my_colleague;
 
 --5
-SELECT * FROM bd_employees where (REGEXP_COUNT(last_name,'[^AEOUIYaeouiy\-]',1,'i')
-= 2 * REGEXP_COUNT(last_name,'[AEOUIYaeouiy]',1,'i'));
+SELECT * FROM bd_employees where (REGEXP_COUNT(last_name,'[qwrtpsdfghjklzxcvbnm\-]',1,'i')
+= 2 * REGEXP_COUNT(last_name,'[aeouiy]',1,'i'));
+
+
+
+ 
+SELECT last_name,
+	REGEXP_REPLACE(
+		REGEXP_REPLACE(
+			REGEXP_REPLACE(
+				REGEXP_REPLACE(
+					REGEXP_REPLACE(
+						REGEXP_REPLACE(
+							REGEXP_REPLACE(last_name,'y',to_char(ASCII('y'),'RN'),'gi'),
+							'i',to_char(ASCII('i'),'RN'),'gi'),
+						'i',to_char(ASCII('i'),'RN'),'gi'),
+					'u',to_char(ASCII('u'),'RN'),'gi'),
+				'o',to_char(ASCII('o'),'RN'),'gi'),
+			'e',to_char(ASCII('e'),'RN'),'gi'),'a',to_char(ASCII('a'),'RN'),'gi')
+	
+FROM bd_employees 
+
 
 --6
 SELECT phone_number,(REGEXP_COUNT(phone_number,'9')*9 + REGEXP_COUNT(phone_number,'8')*8 +
