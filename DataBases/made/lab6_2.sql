c3e2521f46111939987f7861306f3fcf3da8d248
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-12-06 18:20:25 +0300

    Update lab6_2.sql

--- DataBases/made/lab6_2.sql
+++ DataBases/made/lab6_2.sql
@@ -1,14 +1,29 @@
 
 --1а
-with recursive rmanager (employee_id,manager_id) as (
-select employee_id, manager_id from employees where employee_id  = 6 -- ИДшник
+with recursive rmanager (id,manager_id) as (
+select id, manager_id from bd6_employees where id  = 6 -- ИДшник
 union all
-select employees.employee_id, employees.manager_id
-from employees join rmanager on rmanager.manager_id = employees.employee_id
+select bd6_employees.id, bd6_employees.manager_id
+from bd6_employees join rmanager on rmanager.manager_id = bd6_employees.id
 )
-select * from rmanager 
---1b
-create FUNCTION manager_printer() RETURNS integer $$
 select * from rmanager;
-return 1;
-$$
+
+--1b
+CREATE OR REPLACE PROCEDURE manager_printer() as $$
+DECLARE
+i integer;
+emp record;
+BEGIN
+i =6;
+WHILE i >= 1 LOOP
+FOR emp in  SELECT id,manager_id,last_name from bd6_employees where id = i LOOP
+RAISE INFO 	'% %', emp.id, emp.last_name;
+i  = emp.manager_id;
+
+	END LOOP;
+	END LOOP;
+
+END
+	$$
+LANGUAGE plpgsql;
+CALL manager_printer();
