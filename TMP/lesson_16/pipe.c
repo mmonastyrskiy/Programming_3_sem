dbdba873eab45c081f0d17640fb7d02fe11d36e0
mmonastyrskiy <56514528+mmonastyrskiy@users.noreply.github.com>
2022-12-09 07:53:19 +0300

    Pipe sigsev

--- TMP/lesson_16/pipe.c
+++ TMP/lesson_16/pipe.c
@@ -3,42 +3,61 @@
 #include <error.h>
 #include <unistd.h>
 #include <math.h>
+ #include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #define N 4
-float factor(float a, float b){}
+float f(float x ){
+    return sin(x);
+}
+float intrgral(float a, float b){
+  int i; 
+  double Integral; 
+  double h = 0.001;
+ 
+  double n; 
+ 
+  n = (b - a) / h;
+  Integral = 0.0;
+  for(i = 1; i <= n; i++)
+    Integral = Integral + h * f(a + h * (i - 0.5));
+    return Integral;
+}
 
 int main(int argc, const char* argv[]){
-    float global_a, global_b,a,b,step;
+    float global_a, global_b,a,b,step,sum;
     pid_t pid[N];
     int pipefd[N][2];
-    int i,sum;
+    char buff[20];
+    float r;
+    int i,status;
     sum =0;
     if (argc != 3 )
     {
         printf("Bad args");
-        perror(1);
+        perror("1");
 
     }
     global_a = atof(argv[1]);
     global_b = atof(argv[2]);
     step = fabs(global_a-global_b)/N;
     for(i=0;i<N;i++){
-        if(pipefd[i]==-1){
+        if(pipe(pipefd[i])==-1){
             printf("pipe");
-            perror(2);
+            perror("2");
         }
 
         a = global_a + step * i;
         b = global_b - step*(N-1-i);
         pid[i] = fork();
         if(pid[i]==-1){
-            perror(3);
+            perror("3");
         }
         if(pid[i]==0){
             close(pipefd[i][0]);
-            float r;
-            char buff[20];
-         r=factor(a,b);
-        write(pipefd[i][1],sprintf(buffer,"%f",r));
+         r=intrgral(a,b);
+         sprintf(buff,"%f",r);
+        write(pipefd[i][1],buff,20);
         }
 
 
@@ -48,9 +67,20 @@ int main(int argc, const char* argv[]){
 
     for(i = 0; i< N ;i++){
         if(pid[i] != 0){
-            if(waitpid(pid[i],NULL,0)> 0){
-            sum +=atof(read(pipefd[i][0]));
-            close(pipefd[i[1]]);
+            if(waitpid(pid[i],&status,WNOHANG)> 0){
+            read(pipefd[i][0],buff,20);
+            sum += atof(buff);
+            close(pipefd[i][1]);
+        }
+        else{
+            if(status == ECHILD){
+                printf("No child exists");
+                break;
+            }
+            else{
+                printf("Critical error %d", status);
+                exit(status);
+            }
         }
     }
     }
